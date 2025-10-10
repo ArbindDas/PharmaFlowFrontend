28e5ae4e6257eb8a823007ea25e923c26cef3074
Arbind Das <dasarbind269@gmail.com>
2025-10-10 08:45:15 +0545

    text updated

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -53,7 +53,7 @@ const Footer = () => {
                     <svg className="h-4 w-4 text-green-400 mr-2" fill="currentColor" viewBox="0 0 20 20">
                       <path fillRule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clipRule="evenodd" />
                     </svg>
-                    <span className="text-xs text-green-400 font-medium">Licensed</span>
+                    <span className="text-xs text-green-400 font-medium">Licensed Term</span>
                   </div>
                   <div className="flex items-center bg-blue-500/10 border border-blue-500/20 rounded-full px-3 py-1">
                     <svg className="h-4 w-4 text-blue-400 mr-2" fill="currentColor" viewBox="0 0 20 20">
